3c87ec1796d0a143fb6f9fbbde16f05731b2852d
felipe-lopez <cfelipe2005lv@gmail.com>
2025-05-25 12:59:18 -0500

    create generate order

--- src/app/components/product/services/product.service.ts
+++ src/app/components/product/services/product.service.ts
@@ -3,40 +3,28 @@ import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment} from '../../../../enviroments/enviroment.prod';
 
-/*
-export interface Product {
-  nombre: String;
-  descripcion: String;
-  precio: number;
-  imagen: String;
+export interface TypeEgg{
+  id: number;
+  type: string;
 }
- */
+
+export interface Supplier{
+  id: number;
+  name: string;
+  address: string;
+  typeEggs: TypeEgg[];
+}
 
 export interface Product {
   id: number;
-  buyPrice: number;
+  type: TypeEgg;
   color: string;
-  expirationDate: string;
-  inventory: {
-    id: number;
-    nameProduct: string;
-    entryDate: string;
-    totalQuantity: number;
-  };
-  avibleQuantity: number;
+  buyPrice: number;
   salePrice: number;
-  supplier: {
-    id: number;
-    name: string;
-    address: string;
-    typeEggs: { id: number, type: string }[];
-  };
-  type: {
-    id: number;
-    type: string;
-  };
-  imagen: string;
-  nombre: string;
+  expirationDate: string; //(yyyy-mm-dd)
+  supplier: Supplier;
+  avibleQuantity: number;
+  cantidad: number;
 }
 
 @Injectable({
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,31 +1,98 @@
+// cart.component.ts
 import { Component } from '@angular/core';
-import {AuthService} from '../../core/auth.service';
-import {CommonModule} from '@angular/common';
-import {CartService, CartItem} from './services/cart.service';
+import { CommonModule } from '@angular/common';
+import {CartItem, CartService, Order} from './services/cart.service';
+import { AuthService } from '../../core/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cart',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
-  cartItems: CartItem[]= [];
+  cartItems: CartItem[] = [];
+  private userId: number | null = null;
+  order: Order = this.resetOrder();
 
-  constructor(
-    public auth: AuthService,
-    public cartService: CartService
-  ) {
-    this.cartService.cartItems$.subscribe(items =>{
-      this.cartItems = items;
-    });
+  constructor(public auth: AuthService, private cartService: CartService) {
+    try {
+      const user = this.auth.getUserFromToken();
+      this.auth.getUserData(user.sub).subscribe(data => {
+        this.userId = data.id;
+
+        const storageKey = this.getStorageKey();
+        const savedCart = storageKey ? localStorage.getItem(storageKey) : null;
+
+        if (savedCart) {
+          this.cartItems = JSON.parse(savedCart);
+        }
+      });
+    } catch (error) {
+      console.error('Error initializing cart', error);
+      this.userId = null;
+    }
   }
 
-  getTotal(): number{
+
+  save(){
+
+    const storageKey = this.getStorageKey();
+    const savedCart = storageKey ? localStorage.getItem(storageKey) : null;
+
+    if (savedCart) {
+      this.cartItems = JSON.parse(savedCart);
+      this.order = this.resetOrder();
+      console.log(this.order);
+      this.cartService.saveOrder(this.order).subscribe({
+        next: () => {
+          Swal.fire('¡Éxito!', 'Producto guardado correctamente.', 'success');
+          this.resetOrder();
+        },
+        error: (error) => {
+          this.handleError(error, 'guardar');
+        }
+      });
+    }
+  }
+
+  private handleError(error: any, action: string): void {
+    const mensaje = error?.error?.message || `Ocurrió un error al ${action} el producto.`;
+    Swal.fire('¡Error!', mensaje, 'error');
+  }
+
+  private getStorageKey(): string | null {
+    return this.userId !== null ? `cart_${this.userId}` : 'cart_guest';
+  }
+
+  getTotal(): number {
     return this.cartItems.reduce((total, item) => total + (item.price * item.quantity), 0);
   }
 
   removeItem(index: number): void {
-    this.cartService.removeItem(index);
+    this.cartItems.splice(index, 1);
+    this.saveCart();
+  }
+
+  private saveCart(): void {
+    const storageKey = this.getStorageKey();
+    if (storageKey) {
+      localStorage.setItem(storageKey, JSON.stringify(this.cartItems));
+    }
+  }
+
+  resetOrder() {
+    return {
+      idCustomer: this.userId,
+      cartItem: this.cartItems,
+      totalPrice: this.cartItems.reduce(
+        (total, item) => total + (Number(item.price) * Number(item.quantity)),
+        0
+      ),
+      orderDate: new Date().toISOString(), // o cualquier formato que uses
+      state: "PENDIENTE"
+    };
   }
 }
