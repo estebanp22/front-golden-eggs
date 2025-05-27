fa8e555d8b18e341541391784a41da5ea9dd45d1
Esteban Pineda <estebanpinedaardila2210@gmail.com>
2025-05-26 22:32:41 -0500

    Employee module creation, implementation, bug fixes, design improvements

--- src/app/guards/employee.guard.ts
+++ src/app/guards/employee.guard.ts
@@ -2,17 +2,18 @@ import {CanActivate, Router} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {AuthService} from '../core/auth.service';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class EmployeeGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
     const user = this.authService.getUserFromToken();
 
-    if (user && user.role === 'ADMIN') {
+    if (user && user.role === 'EMPLOYEE') {
       return true;
     }
 
